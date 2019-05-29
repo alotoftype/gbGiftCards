15ffb59da7820916b52f29cbb93e6bc32fa1f5e7
serge-tj <serge@tommyjohn.com>
2019-05-28 22:58:19 -0400

    Experimentation with creating a styled component

---
+++ src/components/container.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { box } from 'rebass';
+
+const Container = styled(Box)`
+  max-width: 1280px;
+`
+
+export const Section props => (
+    <Container>
+    
+    </Container>
+)
